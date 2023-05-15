041db5b94ce713cc6f6a7d33b4cdff8acda148dd
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-15 10:25:46 +0530

    skeleton1.cpp
    1

---
+++ Basics of c++/skeleton1.cpp
@@ -0,0 +1,21 @@
+//Skeleton of C++ program
+
+#include <iostream>
+
+
+int main(){
+
+return 0;
+}
+
+//First program
+
+#include <iostream>
+using namespace std;//for output
+
+int main(){
+
+cout<<"Hello world"; //c-compiler
+
+return 0;
+}
