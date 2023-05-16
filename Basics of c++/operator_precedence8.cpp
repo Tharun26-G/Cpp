e453d9207850030fa248d90612ba48c43ee8f223
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-16 06:42:23 +0530

    operator_precedence8.cpp

---
+++ Basics of c++/operator_precedence8.cpp
@@ -0,0 +1,15 @@
+// operator      precedence 
+// ()            High-first 
+// *,/,%         second
+// +,-           Low-Third 
+
+
+// for square root , we have to use header file 
+// #include<cmath>
+
+// sqrt - square root function 
+
+
+// for the power function 
+
+// s=pow(v,2) ----> v^2
