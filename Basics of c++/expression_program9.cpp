97609f8dfac9ffb3dc17459ab9fbdeb5e6a2db1b
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-16 06:46:53 +0530

    expression_program9.cpp

---
+++ Basics of c++/expression_program9.cpp
@@ -0,0 +1,17 @@
+//area of traingle 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float b,h;
+    float a;
+cout<<"Enter b and h";
+cin>>b>>h;
+
+a=(b*h)/2;
+cout<<a;
+
+return 0;
+}
