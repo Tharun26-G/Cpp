d599af141ae784bf2f487a41b40a75860867d9e8
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-16 06:24:55 +0530

    variables4.cpp

---
+++ Basics of c++/variables4.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+  int rollno = 10; //rollno-variable (declared before the use)
+  char group= 'A';
+  float price=12.75f;//f is not necessary 
+  
+  
+}
+
+// Rules
+
+// 1. Number should not be in the beginning 
+// 2. There is no space between the words 
+// 3. Give the valid and approriate name to the variable
