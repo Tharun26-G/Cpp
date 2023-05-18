7a17862ef567330d4574e59b8dd60adbb5101162
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-18 08:25:48 +0530

    Update 06.factor_of_num.cpp

--- Loops/06.factor_of_num.cpp
+++ Loops/06.factor_of_num.cpp
@@ -15,3 +15,25 @@ if(n%i==0)
 
     return 0;
 }
+
+
+//sum of facctor
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n,i,sum=0;
+  cout<<"enter n";
+  cin>>n;
+ for(i=1;i<=n;i++){
+
+if(n%i==0){
+    sum=sum+i;
+}
+
+ }
+cout<<sum;
+
+    return 0;
+}
