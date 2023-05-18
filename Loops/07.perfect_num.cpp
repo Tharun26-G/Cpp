0865b606e5da0927cf14d456f96f86749d0b8e3e
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-18 08:32:47 +0530

    Update 07.perfect_num.cpp

--- Loops/07.perfect_num.cpp
+++ Loops/07.perfect_num.cpp
@@ -1,1 +1,26 @@
+//perfect number is sum of factor of a number should be equal to twice of a number
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n,i,sum=0;
+  cout<<"enter n";
+  cin>>n;
+ for(i=1;i<=n;i++){
+
+if(n%i==0){
+    sum=sum+i;
+}
+ }
+
+ if(2*n=sum){
+    cout<<"perfect number";
+ }
+ else{
+    cout<<"not a perfect number";
+ }
+
+    return 0;
+}
 
