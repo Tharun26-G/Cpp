5486e41307a70c8bea543b75c361c48c7e798d37
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-18 09:32:54 +0530

    Update 14.ex_palindrome.cpp

--- Loops/14.ex_palindrome.cpp
+++ Loops/14.ex_palindrome.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n,r,rev=0,m;
+  cout<<"enter n";
+  cin>>n;
+m=n;
+  while(n>0){
+    r=n%10;
+    n=n/10;
+    rev=rev*10+r;
+  }
 
+  cout<<rev;
+if(rev==m)
+    cout<<"number is palindrome";
+else
+    cout<<"not a palindrome";
+    return 0;
+}
