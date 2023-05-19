c8328d307447f6d440aa61c876499bc183ad89e0
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-19 09:00:12 +0530

    Update 10.multidimensional_array.cpp

--- Arrays/10.multidimensional_array.cpp
+++ Arrays/10.multidimensional_array.cpp
@@ -16,3 +16,21 @@ int main()
     }
     return 0;
 }
+
+//for each 
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int A[2][3]={{2,4,6},{3,6,7}};
+    for(auto& x:A)//rows
+    {
+        for(auto& y:x)//colums
+        {
+            cout<<y<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
