6886d8fe5221529a2e6f9e3304d3168b86565547
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-19 09:08:57 +0530

    Update 11.matrix.cpp

--- Arrays/11.matrix.cpp
+++ Arrays/11.matrix.cpp
@@ -27,6 +27,7 @@ int main()
     int A[2][3] = {3, 3, 3, 3, 3, 3};
     int B[2][3] = {1, 1, 1, 1, 1, 1};
     int C[2][3];
+    //adding and storing for loop
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -36,6 +37,8 @@ int main()
     }
     cout << endl;
     cout << "Sum of 2 matrices comes out to be : " << endl;
+    
+    //display for loop 
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -48,3 +51,36 @@ int main()
     return 0;
 }
 
+
+//subtraction 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A[2][3] = {3, 3, 3, 3, 3, 3};
+    int B[2][3] = {1, 1, 1, 1, 1, 1};
+    int C[2][3];
+    //adding and storing for loop
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            C[i][j] = A[i][j] - B[i][j];
+        }
+    }
+    cout << endl;
+    cout << "sub of 2 matrices comes out to be : " << endl;
+    
+    //display for loop 
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << C[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+}
