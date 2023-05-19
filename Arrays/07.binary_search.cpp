58298e5a6a3021f8a017c8a3b482dfb2f74c1a04
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-19 07:45:50 +0530

    Update 07.binary_search.cpp

--- Arrays/07.binary_search.cpp
+++ Arrays/07.binary_search.cpp
@@ -1,1 +1,29 @@
 //condition for binary search -- should be in sorted array  
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  int A[5]={1,2,3,4,5};
+
+  int l=0,h=4,key,mid;//l=initial array index, h= max array index
+  cout<<"enter key";
+  cin>>key;
+
+  while(l<=h){
+    mid=(l+h)/2;
+    if(key==A[mid]){
+        cout<<"Found at"<<mid;
+        return 0;
+
+    }
+    else if(key<A[mid])
+        h=mid-1;
+
+else
+    l=mid+1;
+  }
+cout<<"not found";
+
+    return 0;
+}
