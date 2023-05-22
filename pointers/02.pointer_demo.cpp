8eaa792ff456f62e3729350290fad492295bac8f
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-22 19:35:09 +0530

    Update 02.pointer_demo.cpp

--- pointers/02.pointer_demo.cpp
+++ pointers/02.pointer_demo.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a=10;
+   int *p=&a;
+
+   cout<<a<<endl;//10
+   cout<<&a<<endl;//addr of a
+   cout<<p<<endl;//addr of a
+   cout<<&p<<endl;//addr of p
+   cout<<*p<<endl;//10
+    return 0;
+}
 
