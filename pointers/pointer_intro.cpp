d6bf449d1325ba3730b71588b10d898d94d23f39
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-22 19:26:39 +0530

    Create pointer_intro.cpp

---
+++ pointers/pointer_intro.cpp
@@ -0,0 +1,11 @@
+/*Pointer is a variable used to store an address of an data 
+It has 2 types 
+1.Data varaible        int x=10;
+2.Address varaible     int *p;
+                       p=&x; //it having the address of x, not the value of x.
+                       
+ declaration        int *p;
+ intialization      p=&x;
+ dereferencing      cout<<*p; //going to the reference and accessing the data 
+ 
+ */
