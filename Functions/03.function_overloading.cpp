70f4d26c56f8320ea4b91f08a02d0f1dd0edaffd
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-05-24 08:11:42 +0530

    Create 03.function_overloading.cpp

---
+++ Functions/03.function_overloading.cpp
@@ -0,0 +1,20 @@
+// Having same function but different paramter is known as function overloading 
+  #include<iostream>
+
+  using namespace std;
+  int add(int x,int y){
+  return x+y;
+  }
+
+int add(int x,int y,int z){
+return  x+y+z;
+}
+  int main(){
+      int x=10,y=23,z,r;
+z=add(x,y);//33
+cout<<z<<endl;
+r=add(x,y,z);//66
+cout<<r;
+
+  }
+
