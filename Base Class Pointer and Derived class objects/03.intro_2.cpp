47f6fccdee75544fe4c09df75280f770031bdff2
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-06-09 07:34:11 +0530

    Create 03.intro_2.cpp

---
+++ Base Class Pointer and Derived class objects/03.intro_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle{
+
+public:
+    void area()
+    {
+        
+        cout<<"area of rect"<<endl;
+        
+    }
+};
+
+class Cuboid:public Rectangle
+{
+public:
+    void volume(){
+    cout<<"cuboid volume"<<endl;
+    }
+};
+int main()
+{
+    Cuboid c;
+    /* Able to call both of the functions
+    c.area();
+    c.volume();
+     */
+    // Creating pointer to rectangle class and to this we will assign the address of it's child class object i.e. c
+    Rectangle *p = &c;
+    p->area(); // Able to call the function area as it is defined in the class Rectangle.
+    // p->volume(); // Gives error that Rectangle has no member volume()
+    return 0;
+}
