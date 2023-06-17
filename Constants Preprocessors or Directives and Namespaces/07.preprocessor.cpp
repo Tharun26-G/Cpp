e02a5c3bc2b193cfe837a114af3e5aba178dc068
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2023-06-17 06:59:47 +0530

    Update 07.preprocessor.cpp

--- Constants Preprocessors or Directives and Namespaces/07.preprocessor.cpp
+++ Constants Preprocessors or Directives and Namespaces/07.preprocessor.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+using namespace std;
+
+#define PI 3.14
+#define c cout
+
+
+int main(){
+    cout<<PI<<endl; //PI is defined as 3.14 
+
+    c<<10; //c is defined as cout 
+
+}
